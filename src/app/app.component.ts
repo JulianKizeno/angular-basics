4d73173030ba01ee4fe0557dcea46365894ab676
Julian Quiceno <julian.quiceno@accenture.com>
2023-03-19 22:57:32 +0100

    feat(angular_basics): NgClass NgStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my_store';
+  name = 'Julian';
+  age = 32;
+  img = 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Prescription01&hairColor=Brown&facialHairType=BeardLight&facialHairColor=Brown&clotheType=CollarSweater&clotheColor=Black&eyeType=Wink&eyebrowType=RaisedExcited&mouthType=Twinkle&skinColor=Pale';
+  btnDisabled = true;
+  person = {
+    name: 'Julian',
+    age: 32,
+    avatar: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Prescription01&hairColor=Brown&facialHairType=BeardLight&facialHairColor=Brown&clotheType=CollarSweater&clotheColor=Black&eyeType=Wink&eyebrowType=RaisedExcited&mouthType=Twinkle&skinColor=Pale'
+  };
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  }
 }
